6398285611553e34eec4c39251e662c82dc09ccd
gddaredevil <root@localhost.localdomain>
2021-03-21 11:53:19 +0530

    modified:   pycat.py 	new file:   pycat_util.py

--- project_10/pycat.py
+++ project_10/pycat.py
@@ -1,83 +1,84 @@
-import socket
 import sys
-import os
-from pycat_chat import tcp_connect
-import signal
+import socket
+import threading
+import argparse
+import pycat_util
+from signal import signal
 
-class Netcat:
-    def __init__(self, argv):
-        self.noAg = len(argv)
-        self.args = list(argv)
-    def chat(self,ip,port,status):
-        print("Chat Initiated")
-        ins = tcp_connect(str(ip),str(port))
-        if(status=='serv'):
-            ins.serv()
-        elif(status=='cli'):
-            ins.cli()
-        
-def areFlags(arg):
-    flagList=[]
-    for i in arg[1:-2]:
-        if(isFlag(i)):
-            flagList.append(i)
-    return(flagList)
-def isFlag(arg):
-    if(len(arg)==2):
-        if(arg[0]=='-' and arg[1].isalpha()==True):
-            return True
-    return False
-def getIP():
+#Global Variables
+Listen = False
+Connect= False
+PortScan=False
+
+def usage():
+    print("Usage:")
+    print("Connect to somewhere: \t python3 pycat.py [-options] hostname")
+    print("Listen for connections: \t python3 pycat.py -l -p port [-options]..")
+    print("-p \t port \t : Specify port for establishing a connection")
+    print("-l \t listen \t: Listen for incoming connections")
+    print("-v \t Verbose \t : Display verbose output")
+    print("-z \t Zero mode \t : Used for scanning ports")
+    sys.exit(1)
+
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', dest='listen', help="Listen for incoming connections",action='store_true')
+    parser.add_argument('-p', '--port',help="specify port to listen on")
+    parser.add_argument("host_address", nargs="*")
+    parser.add_argument('-v', help="Verbose Output", action="store_true")
+    parser.add_argument('-z', help="Zero I/O mode - Used for Scanning", action="store_true")
+    args= vars(parser.parse_args())
+    return(args)
+
+def isIP(addr):
     try:
-        return([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-    except:
-        return('127.0.0.1')
-def isAddress(arg):
-    numflag=0
-    if(arg.count('.')==3):
-        iplist=arg.split('.')
-        if(len(iplist)==4):
-            for i in iplist:
-                if(i.isdigit() and int(i)>=0 and int(i)<256):
-                    pass
-                else:
-                    return False
-        else:
+        seg = addr.split('.')
+        if(len(seg)!=4):
             return False
-    else:
+        for i in seg:
+            if(int(i)<0 or int(i)>255):
+                return False
+        return True
+    except:
         return False
-    return True
-def isPort( arg):
-    if(arg.isdigit()):
-        if(int(arg)>1024 and int(arg)<65536):
-            return(True)
-    return(False)
 
+def main():
 
+    global Listen   
+    global Connect
+
+    opts = argparser()
+    if(len(sys.argv)<2):
+        usage()
+
+    #Listen
+    Listen = True if opts['listen']==True else False
+    if(Listen):
+        if(opts['port'] == None):
+            usage()
+        else:
+            pycat_util.listen(opts['port'])
+
+    #Connect
+    Connect = True if opts['listen']==False and opts['z']==False and opts['host_address']!=[] else False
+#    print(opts['host_address'], len(opts['host_address']))
+    if(Connect):
+        ip = opts['host_address'][0]
+        port = int(opts['port'])
+        if(isIP(ip)):
+            pycat_util.connect(ip,port)
+        else:
+            print("Invalid IP address")
+
+    #Port Scanning
+    PortScan = True if opts['z']==True else False
+    if(PortScan):
+        port=opts['port']
+        ip = opts['host_address'][0] if isIP(opts['host_address'][0]) else None
+        if(ip):
+            pycat_util.portScan(ip,port)
+        else:
+            print("Invalid IP")
 
 if __name__=='__main__':
-    instance = Netcat(sys.argv)
-    args = list(sys.argv)
-
-    if(len(args) < 2):
-        print("Usage:")
-        print("python3 pycat.py <options> host_address port")
-        print(" -l -p <port>\t : listen on a particular port")
-        print("")
-        sys.exit(1)
-
-    if('-l' not in args):
-        if(isAddress(args[-2])):
-            if(isPort(args[-1])):
-                instance.chat(str(args[-2]),str(args[-1]),'cli')
-            else:
-                print("Port error")
-                sys.exit(1)
-        else:
-            print("Network Unreachable. Invalid Ip address")
-            sys.exit(1)
-    else:
-        if(isFlag(args[-2]) and args[-2]=='-p'):
-            if(isPort(args[-1])):
-                ip_ad=getIP()
-                instance.chat(str(ip_ad),str(args[-1]),'serv')
+    main()
---
+++ project_10/pycat_util.py
@@ -0,0 +1,124 @@
+import socket
+import threading
+import sys
+import logging
+import time
+from signal import signal
+
+logging.basicConfig(format="    %(asctime)s : %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+
+def getIP():
+    try:
+        return([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+    except:
+        return(socket.gethostbyname(socket.gethostname()))
+def send(s):
+    while(True):
+        msg=input()
+        s.send(msg.encode())
+
+def recv(s):
+    while(True):
+        msg = s.recv(1024)
+        if not msg:
+            break
+        print(msg.decode())
+
+def signal_handler(sign_recv, frame):
+    print("Level 2")
+
+def listen(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip=getIP()
+    s.bind((ip, int(port)))
+    s.listen(5)
+    stat, conn = s.accept()
+    print('{} established a connection through port {}'.format(conn[0],conn[1]))
+    sen = threading.Thread(target=send, args=(stat,), daemon=False)
+    rec = threading.Thread(target=recv, args=(stat,), daemon=False)
+#    sen.daemon=True#    rec.daemon=True
+    sen.start()
+    rec.start()
+
+def connect(ip, port):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip,port))
+    print("Connection Successful")
+    sen = threading.Thread(target=send, args=(s,), daemon=False)
+    rec = threading.Thread(target=recv, args=(s,), daemon=False)
+
+    sen.start()
+    rec.start()
+
+def portScan(ip, port):
+    start_time = time.time()
+    socket.setdefaulttimeout(1)
+    counter=0
+    inc=35
+    threads=[]
+    flag="CREATE"
+    port_ini=0
+    port_fin=1024
+    print(port)
+    if(port.isdigit() == False):
+        try:
+            arr = port.split('-')
+            if(len(arr)!=2 or arr[0].isdigit()==False or arr[1].isdigit()==False):
+                print("Invalid port")
+                sys.exit(1)
+            else:
+                port_ini=int(arr[0])
+                port_fin=int(arr[1])+1
+        except:
+            print("Invalid input for Port")
+    else:
+        port_ini = int(port)
+        port_fin = int(port)+1
+    while(port_ini < port_fin):
+        if(flag=="CREATE"):
+            counter+=1
+            pini = port_ini
+            if(port_ini+inc < port_fin):
+                pfin = pini+inc
+                port_ini = pfin
+            else:
+                pfin = port_ini+(port_fin-port_ini)
+                port_ini= pfin
+            t= threading.Thread(target=scan, args=(ip, pini, pfin,))
+            threads.append(t)
+            t.start()
+            if(counter>=128):
+                flag="DESTROY"
+        elif(flag=="DESTROY"):
+            for index,thread in enumerate(threads):
+                print("Thread {} is joining...".format(thread))
+                thread.join()
+                counter-=1
+                if(counter<=0):
+                    flag="CREATE"
+    for index, thread in enumerate(threads):
+        thread.join()
+    end_time=time.time()
+    print("[^_^] Scan completed in {:.2f} seconds".format(end_time-start_time))
+def scan(ip,pini,pfin):
+    for i in range(pini, pfin):
+        ip = socket.gethostbyname(ip)
+        try:
+#            print("Connecting to Port {}".format(i))
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            res=s.connect_ex((ip,i))
+            if(res==0):
+                print("[*] Port {}/{} \t open".format(i,socket.getservbyport(i)))
+            s.close()
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt Detected. Exiting...")
+            sys.exit(1)
+        except socket.gaierror:
+            print("Hostname couldn't be resolved. Exiting...")
+            sys.exit(1)
+        except socket.error:
+#            print("Server not responding. Exiting...")
+            print('')
+            sys.exit(1)
+
+
